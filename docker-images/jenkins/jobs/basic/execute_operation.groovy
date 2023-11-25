93519733b6617a4c6f97974749be553a5efb2262
nangashi <tagjmp@gmail.com>
2023-11-25 23:10:59 +0900

    operationお試し

--- docker-images/jenkins/jobs/basic/execute_operation.groovy
+++ docker-images/jenkins/jobs/basic/execute_operation.groovy
@@ -1,40 +1,18 @@
-pipelineJob("execute-operation") {
-	description()
-	parameters {
-		activeChoiceParam("script") {
-			groovyScript {
-				script("""import java.io.File
-
-// 特定のディレクトリパスを指定
-def dirPath = '/var/jenkins_home/workspace/prepare-operation/jenkins-operation'
-
-// ディレクトリ内のファイルをリスト化
-def dir = new File(dirPath)
-def fileList = []
-
-// ディレクトリが存在し、ディレクトリであるか確認
-if (dir.exists() && dir.isDirectory()) {
-    // 拡張子が .groovy のファイルをリスト化
-    dir.eachFile { file ->
-        if (file.name.endsWith('.groovy')) {
-            // 拡張子を除いたファイル名をリストに追加
-            fileList.add(file.name[0..-8]) // ".groovy" は7文字なので、最後から7文字を除去
+pipelineJob('execute-operation') {
+  parameters {
+    stringParam('JENKINSFILE', '', 'Jenkinsfile script for operation.')
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/nangashi/dcr.git')
+          }
+          branches('*/feature/jenkins')
+          scriptPath('jenkins-operation/${JENKINSFILE}.groovy')
         }
+      }
     }
-}
-
-// ファイル名のリストを返す
-return fileList
-""")
-				fallbackScript("")
-			}
-			choiceType("SINGLE_SELECT")
-			filterable(false)
-		}
-	}
-// 	definition {
-// 		cpsScm {
-// ""		}
-// 	}
-	disabled(false)
+  }
 }
---
+++ docker-images/jenkins/script/connect-db.groovy
@@ -0,0 +1,33 @@
+package com.example
+
+import groovy.sql.Sql
+
+class MySqlHelper {
+  private Sql sql
+
+  MySqlHelper() {
+    // データベース接続設定
+    String url = 'jdbc:mysql://your-db-url:3306/your-db'
+    String user = 'your-username'
+    String password = 'your-password'
+    this.sql = Sql.newInstance(url, user, password, 'com.mysql.cj.jdbc.Driver')
+  }
+
+  def executeQuery(String query) {
+    if (query.toLowerCase().startsWith('select')) {
+      return this.sql.rows(query)
+    } else {
+      throw new IllegalArgumentException('Query must be a SELECT statement.')
+    }
+  }
+
+  def executeUpdate(String query) {
+    if (!query.toLowerCase().startsWith('select')) {
+      return this.sql.execute(query)
+    } else {
+      throw new IllegalArgumentException('Query must be INSERT, UPDATE, or DELETE.')
+    }
+  }
+
+  // トランザクション処理のメソッドは必要に応じて追加
+}
