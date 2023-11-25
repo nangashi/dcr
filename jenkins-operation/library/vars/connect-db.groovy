22b99079079e259ab911e3cb42ba30c5f4ff01ec
nangashi <tagjmp@gmail.com>
2023-11-25 23:34:42 +0900

    test

--- jenkins-operation/library/vars/connect-db.groovy
+++ jenkins-operation/library/vars/connect-db.groovy
@@ -2,17 +2,17 @@ package com.example
 
 import groovy.sql.Sql
 
-class MySqlHelper {
-  // private Sql sql
+// class MySqlHelper {
+//   // private Sql sql
 
-  // MySqlHelper() {
-  //   // データベース接続設定
-  //   String url = 'jdbc:mysql://your-db-url:3306/your-db'
-  //   String user = 'your-username'
-  //   String password = 'your-password'
-  //   // this.sql = Sql.newInstance(url, user, password, 'com.mysql.cj.jdbc.Driver')
-  //   this.sql = null
-  // }
+//   // MySqlHelper() {
+//   //   // データベース接続設定
+//   //   String url = 'jdbc:mysql://your-db-url:3306/your-db'
+//   //   String user = 'your-username'
+//   //   String password = 'your-password'
+//   //   // this.sql = Sql.newInstance(url, user, password, 'com.mysql.cj.jdbc.Driver')
+//   //   this.sql = null
+//   // }
 
   def executeQuery(String query) {
     if (query.toLowerCase().startsWith('select')) {
@@ -32,4 +32,4 @@ class MySqlHelper {
   }
 
   // トランザクション処理のメソッドは必要に応じて追加
-}
+// }
