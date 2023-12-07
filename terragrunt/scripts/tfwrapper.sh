524a9b413962e52c43388cae0496df84a9c4fab7
nangashi <tagjmp@gmail.com>
2023-12-07 00:05:20 +0000

    local notification

--- terragrunt/scripts/tfwrapper.sh
+++ terragrunt/scripts/tfwrapper.sh
@@ -2,26 +2,26 @@
 
 set -eu
 
-TERRAFORM_OUTPUT_FILE=./terraform.log
-
-terraform "$@" -no-color 2>&1 | tee ${TERRAFORM_OUTPUT_FILE}
+tf_output=$(terraform "$@" -no-color 2>&1)
 
 type=$(echo "$@" |  awk '{print $1}')
 
 if [ "$type" != "plan" ] && [ "$type" != "apply" ]; then
+  echo "$tf_output"
   exit 0
 fi
 
 # 変更がないときは通知しない
-if grep -q "No changes. Your infrastructure matches the configuration." "${TERRAFORM_OUTPUT_FILE}"; then
+if echo "$tf_output" | grep -q "No changes. Your infrastructure matches the configuration."; then
     echo "No changes found. Exiting..."
     exit 0
 fi
-
 # applyを取り消したときは通知しない
-if grep -q "Apply cancelled." "${TERRAFORM_OUTPUT_FILE}"; then
+if echo "$tf_output" | grep -q "Apply cancelled."; then
     exit 0
 fi
 
 current_module=$(basename $(pwd))
-tfnotify --config "${TFNOTIFY_CONFIG}" "$type" --message "Terraform $TF_VAR_env - $current_module" < ${TERRAFORM_OUTPUT_FILE}
+echo "$tf_output" \
+  | sed '/Terraform will perform the following actions/,$!d' \
+  | tfnotify --config "${TFNOTIFY_CONFIG}" "$type" --message "Terraform $TF_VAR_env - $current_module"
