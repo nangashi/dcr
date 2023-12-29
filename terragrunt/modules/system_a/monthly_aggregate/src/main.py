ca2b49c1b5b941b923ef59873510c3d259c6fbb3
nangashi <tagjmp@gmail.com>
2023-12-29 23:37:18 +0000

    system-a-2

--- terragrunt/modules/system_a/monthly_aggregate/src/main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # ここに処理を記述する
-    print("hogera2")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
